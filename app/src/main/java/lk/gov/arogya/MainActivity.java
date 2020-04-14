8416689f58e53c609a37be9d59b477541f8e703d
Anjana Senanayake <anjanajayabandara@gmail.com>
2020-04-14 11:01:35 +0530

    .gitignore added

---
+++ app/src/main/java/lk/gov/arogya/MainActivity.java
@@ -0,0 +1,14 @@
+package lk.gov.arogya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
