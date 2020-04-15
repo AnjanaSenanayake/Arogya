0569172ebd31bdd4fb6aea814299d156a995da56
Anjana Senanayake <anjanajayabandara@gmail.com>
2020-04-15 19:56:24 +0530

    api added to connect remote database

--- app/src/main/java/lk/gov/arogya/activity/LoginActivity.java
+++ app/src/main/java/lk/gov/arogya/activity/LoginActivity.java
@@ -1,16 +1,25 @@
 package lk.gov.arogya.activity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Toast;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 import lk.gov.arogya.R;
+import lk.gov.arogya.interfaces.NodeJSAPI;
+import lk.gov.arogya.support.RetrofitClient;
+import retrofit2.Retrofit;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity {
 
     private EditText edtNIC, edtPassword;
     private Button btnLogin;
@@ -30,5 +39,38 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
             }
         });
+        btnLogin.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                String nic = edtNIC.getText().toString();
+                String password = edtPassword.getText().toString();
+                login(nic, password);
+            }
+        });
+    }
+
+    private void login(String nic, String password) {
+        Retrofit retrofit = RetrofitClient.getInstance();
+        NodeJSAPI nodeJSAPI = retrofit.create(NodeJSAPI.class);
+        CompositeDisposable compositeDisposable = new CompositeDisposable();
+        compositeDisposable.add(nodeJSAPI.login(nic, password)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(final String s) {
+                        if (s.contains("encrypted_password")) {
+                            Toast.makeText(LoginActivity.this, "Login Success", Toast.LENGTH_LONG).show();
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) {
+                        Log.e(SignUpActivity.class.getSimpleName(), throwable.getMessage(), throwable);
+                    }
+                }));
     }
 }
--- app/src/main/java/lk/gov/arogya/activity/SignUpActivity.java
+++ app/src/main/java/lk/gov/arogya/activity/SignUpActivity.java
@@ -1,16 +1,25 @@
 package lk.gov.arogya.activity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Toast;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 import lk.gov.arogya.R;
+import lk.gov.arogya.interfaces.NodeJSAPI;
+import lk.gov.arogya.support.RetrofitClient;
+import retrofit2.Retrofit;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends Activity {
 
     private EditText edtFullName, edtNIC, edtMobileNumber;
     private EditText edtPassword;
@@ -25,6 +34,7 @@ public class SignUpActivity extends AppCompatActivity {
         edtNIC = findViewById(R.id.edt_nic);
         edtMobileNumber = findViewById(R.id.edt_phone_number);
         edtPassword = findViewById(R.id.edt_password);
+        btnRegister = findViewById(R.id.btn_register);
         tvLinkToLogin = findViewById(R.id.tv_link_to_login);
         tvLinkToLogin.setOnClickListener(new OnClickListener() {
             @Override
@@ -32,5 +42,37 @@ public class SignUpActivity extends AppCompatActivity {
                 startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
             }
         });
+        btnRegister.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                String name = edtFullName.getText().toString();
+                String nic = edtNIC.getText().toString();
+                String mobile = edtMobileNumber.getText().toString();
+                String password = edtPassword.getText().toString();
+                register(name, nic, mobile, password);
+            }
+        });
+    }
+
+    private void register(String name, String nic, String mobile, String password) {
+        Retrofit retrofit = RetrofitClient.getInstance();
+        NodeJSAPI nodeJSAPI = retrofit.create(NodeJSAPI.class);
+        CompositeDisposable compositeDisposable = new CompositeDisposable();
+        compositeDisposable.add(nodeJSAPI.register(name, nic, mobile, password)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(final String s) {
+                        Toast.makeText(SignUpActivity.this, "Register Success", Toast.LENGTH_LONG).show();
+                        finish();
+                        startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) {
+                        Log.e(SignUpActivity.class.getSimpleName(), throwable.getMessage(), throwable);
+                    }
+                }));
     }
 }
